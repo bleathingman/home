592ebccb593c9a046602d87543271ffbf39d1788
Matheo Dourneau <matheo.dourneau@gmail.com>
2022-02-06 22:13:03 +0100

    toto

--- home.php
+++ home.php
@@ -13,7 +13,15 @@
   <?php include_once('header.php'); ?>
 
   <!--Contents-->
-  <h1> toto</h1>
+  <h1>
+    <div class="jumbotron">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+  </h1>
 
 
 </body>
